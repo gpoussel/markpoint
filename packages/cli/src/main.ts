22a0312b0fbaffffac574a9009a173641f34f819
Guillaume POUSSEL <gpoussel@gmail.com>
2024-03-16 09:21:22 +0100

    Initialize monorepo structure
    Currently using Turbo, it sounds like the best way to handle commands
    across several packages.

---
+++ packages/cli/src/main.ts
@@ -0,0 +1,8 @@
+import { APPLICATION_NAME } from '@markpoint/shared'
+
+function main() {
+  // eslint-disable-next-line no-console
+  console.log(`Hello from the app named ${APPLICATION_NAME}`)
+}
+
+main()
