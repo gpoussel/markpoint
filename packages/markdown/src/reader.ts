0b869762eaf2a1fd6d6a7e96cd1936512b1a2a86
Guillaume POUSSEL <gpoussel@gmail.com>
2024-03-21 09:57:19 +0100

    Create a markdown package

---
+++ packages/markdown/src/reader.ts
@@ -0,0 +1,9 @@
+export class MarkdownReader {
+  async read(file: string): Promise<void> {
+    // eslint-disable-next-line no-console
+    console.log('reading file:', file)
+    return new Promise((resolve) => {
+      resolve()
+    })
+  }
+}
