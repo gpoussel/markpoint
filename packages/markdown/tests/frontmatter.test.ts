24b6d402052a20b870628659f2472af51599c396
Guillaume POUSSEL <gpoussel@gmail.com>
2024-03-23 00:44:47 +0100

    Add tests for frontmatter

---
+++ packages/markdown/tests/frontmatter.test.ts
@@ -0,0 +1,65 @@
+import type { Root } from 'mdast'
+import { expect, describe, it } from 'vitest'
+
+import { extractFrontMatter } from '../src/frontmatter'
+
+describe('extractFrontMatter', () => {
+  it('should return undefined if the document is empty', () => {
+    const tree: Root = {
+      type: 'root',
+      children: [],
+    }
+    const result = extractFrontMatter(tree)
+    expect(result).toBeUndefined()
+    expect(tree.children).toHaveLength(0)
+  })
+
+  it('should return undefined if there is no YAML frontmatter', () => {
+    const tree: Root = {
+      type: 'root',
+      children: [
+        {
+          type: 'paragraph',
+          children: [
+            {
+              type: 'text',
+              value: 'This is some content',
+            },
+          ],
+        },
+      ],
+    }
+    const result = extractFrontMatter(tree)
+    expect(result).toBeUndefined()
+    expect(tree.children).toHaveLength(1)
+    expect(tree.children[0].type).toBe('paragraph')
+  })
+
+  it('should parse and return the frontmatter object if YAML frontmatter exists', () => {
+    const tree: Root = {
+      type: 'root',
+      children: [
+        {
+          type: 'yaml',
+          value: 'company: Acme, Inc.\nauthor: John Doe',
+        },
+        {
+          type: 'paragraph',
+          children: [
+            {
+              type: 'text',
+              value: 'This is some content',
+            },
+          ],
+        },
+      ],
+    }
+    const result = extractFrontMatter(tree)
+    expect(result).toEqual({
+      company: 'Acme, Inc.',
+      author: 'John Doe',
+    })
+    expect(tree.children).toHaveLength(1)
+    expect(tree.children[0].type).toBe('paragraph')
+  })
+})
